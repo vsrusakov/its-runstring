574facd4363beb4290e776cc31b70b6c55752e87
Vladimir Rusakov <virt.p.q@yandex.ru>
2024-07-01 15:57:49 +0300

    added model Video

--- runstring/str_loader/models.py
+++ runstring/str_loader/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Video(models.Model):
+
+    user_ip = models.GenericIPAddressField()
+    text = models.TextField()
+    fps = models.PositiveSmallIntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
--- runstring/str_loader/views.py
+++ runstring/str_loader/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import FileResponse
 from video_maker.maker import get_bvideo
+from .models import Video
 import os
 
 # Create your views here.
@@ -15,5 +16,13 @@ def send_video(request):
     name = request.GET.get('name', 'my_video')
     path_to_save = os.path.join('.', 'video_maker', 'videos')
 
+    xff = request.META.get('HTTP_X_FORWARDED_FOR')
+    if xff:
+        user_ip = xff.split(',')[0]
+    else:
+        user_ip = request.META.get('REMOTE_ADDR')
+    Video(user_ip=user_ip, text=text, fps=fps).save()
+
     video = get_bvideo(text, fps, path_to_save, name)
+
     return FileResponse(video, content_type='video/mp4', as_attachment=True)
