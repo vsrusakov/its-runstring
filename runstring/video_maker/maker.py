7b669d7a704fd851e10d54d748cd53c4d55c6fcd
Vladimir Rusakov <virt.p.q@yandex.ru>
2024-07-03 15:59:50 +0300

    bag fixed

--- runstring/video_maker/maker.py
+++ runstring/video_maker/maker.py
@@ -20,11 +20,11 @@ def get_bvideo(text, fps, path_to_save, name):
     # накладываем текст на фоновое изображение и приводим к нужному размеру
     org = (int(higth / 2), int(3 * higth / 2))
     img_w_text = cv2.putText(bg_img, text, org, font_face, font_scale, (255, 255, 255), thickness)
-    target_img = cv2.resize(img_w_text, (int(100 * bg_w / bg_h), 100))
+    target_img = cv2.resize(img_w_text, (max(100, int(100 * bg_w / bg_h)), 100))
 
     # создаем VideoWriter и записываем кадры
     num_frames = fps * 3
-    step = (int(100 * bg_w / bg_h) - 100) / num_frames
+    step = max(0, (int(100 * bg_w / bg_h) - 100) / num_frames)
     video = cv2.VideoWriter(video_path, fourcc, fps, (100, 100))
 
     for i in range(num_frames):
